b18a913ea7c5b2d95a6dedbd5eca613051689158
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2022-02-03 00:07:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const randomMeal = document.getElementById('randomMeal')
 let search = '';
 
 const fetchSearch = async() => {
-    meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata').
+    meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=${search}').
     then(res => res.json()).
     then(res => res.meals);
     console.log(meals);
@@ -13,12 +13,20 @@ const fetchSearch = async() => {
 
 const searchDisplay = async() => {
     await fetchSearch();
+
+    if (meals == null){
+        results.innerHTML = `<span class='noResult'>Aucun resultats</span>`;
+    } else {
+
+    }
+
 }
 
 searchInput.addEventListener('input', (e) => {
     console.log(e.target.value);
-    search = e.target.value
-
+    searchDisplay();  
+    console.log(search);
+ 
 })
 
 fetchSearch();
