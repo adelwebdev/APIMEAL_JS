ed6121bc8cb7a8294d7a39cac78a70a2227891a3
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2022-02-05 12:43:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,43 +4,64 @@ const randomMeal = document.getElementById('randomMeal')
 
 let search = '';
 
-const fetchSearch = async() => {
+const fetchSearch = async(url) => {
     meals = await fetch(
-        'https://www.themealdb.com/api/json/v1/1/search.php?s=tomato')
-        .
-    then(res => res.json()).
-    then(res => res.meals);
+        'https://www.themealdb.com/api/json/v1/1/search.php?s=${url}')
+        .then(res => res.json()).then(res => res.meals);
     console.log(meals);
 } 
 
+
+//search
 const searchDisplay = async() => {
-    await fetchSearch();
+    await fetchSearch(search);
 
     if (meals == null){
-        results.innerHTML = `<span>Aucun resultats</span>`;
+        results.innerHTML = `<span>Aucun resultat</span>`;
     } 
     results.innerHTML = (
         meals.map(meal => (   
           `
-          <div class="searchContainer">
-            <h2>${meal.strMeal}</h2>
-            <div class="infos">
-              <div>origin : ${meal.strArea}</div>
-              <div>category : ${meal.strCategory}</div>
+            <div class="searchContainer">
+                <h2>${meal.strMeal}</h2>
+                <div class="infos">
+                <div>origin : ${meal.strArea}</div>
+                <div>category : ${meal.strCategory}</div>
+                </div>
+                <img src='${meal.strMealThumb}' /></br>
+                <a href="${meal.strYoutube}" target="_blank"><i class="fab fa-youtube"></i></a>
             </div>
-            <img src='${meal.strMealThumb}' /></br>
-            <a href="${meal.strYoutube}" target="_blank"><i class="fab fa-youtube"></i></a>
-          </div>
           `
         )).join('')
-      );
+      ); 
 }
 
 searchInput.addEventListener('input', (e) => {
-    search = e.target.value;
-    searchDisplay();  
-    console.log(search);
- 
+    search = `search.php?s=${e.target.value}`;
+    searchDisplay();
 })
 
-fetchSearch();
+//random meal
+const randomMealDisplay = async() => {
+    await fetchSearch('random.php');
+
+    results.innerHTML = (
+        meals.map(meal => (
+            `
+            <div class="randomContainer">
+              <h2>${meal.strMeal}</h2>
+              <div class="infos">
+                <div>origin : ${meal.strArea}</div>
+                <div>catégory : ${meal.strCategory}</div>
+              </div>
+              <img src='${meal.strMealThumb}' />
+              <p>${meal.strInstructions}</p>
+              <a href="${meal.strYoutube}" target="_blank"><i class="fab fa-youtube"></i></a>
+            </div>
+          `
+        ))
+    )
+}
+
+randomMeal.addEventListener('click', randomMealDisplay);
+randomMealDisplay();
